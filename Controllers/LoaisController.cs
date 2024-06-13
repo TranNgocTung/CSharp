dc5679c3f063f6939ad14cc47237cc252ade7083
TranNgocTung <ngoctung10a6@gmail.com>
2024-06-13 09:13:52 +0700

    data

---
+++ Controllers/LoaisController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MyWebApiApp.Data;
+using MyWebApiApp.Models;
+
+namespace MyWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoaisController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+
+        public LoaisController(MyDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var dsLoai = _context.loais.ToList();
+            return Ok(dsLoai);  
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var dsLoai = _context.loais.SingleOrDefault(
+                lo => lo.MaLoai == id);
+            if (dsLoai == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Ok(dsLoai);
+            }
+        }
+        [HttpPost]
+        public IActionResult CreateNew(LoaiModel model)
+        {
+            try
+            {
+                var loai = new Loai()
+                {
+                    TenLoai = model.TenLoai
+                };
+                _context.Add(loai);
+                _context.SaveChanges();
+                return Ok(loai);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+
+            }
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateLoaiById(int id, LoaiModel model)
+        {
+            var loai = _context.loais.SingleOrDefault( lo => lo.MaLoai == id);
+            if (loai != null)
+            {
+                loai.TenLoai = model.TenLoai;
+                _context.SaveChanges();
+                return Ok(loai);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyWebApiApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Thêm dịch vụ DbContext với chuỗi kết nối từ appsettings.json
+builder.Services.AddDbContext<MyDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDB")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}   
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
